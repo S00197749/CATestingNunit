1effceef7b83f0c6d826628793edeaafd9aa7a30
Luke Sweeney <60261891+lsweeney00@users.noreply.github.com>
2021-04-20 13:15:23 +0100

    Tried to fix Fitnesse

--- CATesting.Tests/ProgramTest.cs
+++ CATesting.Tests/ProgramTest.cs
@@ -14,11 +14,11 @@ namespace CATesting.Tests
         [TestCase(10, "female", ExpectedResult = 0.0)]
         [TestCase(20, "female", ExpectedResult = 5.0)]
         [TestCase(40, "female", ExpectedResult = 2.5)]
-        [TestCase(60, "female", ExpectedResult = 0.375)]
+        [TestCase(60, "female", ExpectedResult = 1.25)]
         [TestCase(10, "male", ExpectedResult = 0.0)]
         [TestCase(20, "male", ExpectedResult = 6.0)]
         [TestCase(40, "male", ExpectedResult = 5.0)]
-        [TestCase(60, "male", ExpectedResult = 0.75)]
+        [TestCase(60, "male", ExpectedResult = 2.5)]
         [TestCase(10, "UNKNOWN", ExpectedResult = 0.0)]
         public static float Test(int age, string gender)
         {
---
+++ MyFixture/MyFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CATesting;
+
+namespace MyFixture
+{
+    public class MyFixture: fit.ColumnFixture
+    {
+        InsuranceService i1 = new InsuranceService();
+        public int age;
+        public string gender;
+
+        public double CalPremium()
+        {
+            return i1.CalcPremium(age, gender);
+        }
+    }
+}
